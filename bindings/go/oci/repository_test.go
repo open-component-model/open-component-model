55499d1834fda282eb55f4dddd4874dc807204da
Jakob Möller <jakob.moeller@sap.com>
2025-04-11 12:43:32 +0200

    chore: dont fail on xl

--- bindings/go/oci/repository_test.go
+++ bindings/go/oci/repository_test.go
@@ -9,7 +9,6 @@ import (
 	"testing"
 
 	"github.com/opencontainers/go-digest"
-	"github.com/opencontainers/image-spec/specs-go"
 	ociImageSpecV1 "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/stretchr/testify/require"
 	"oras.land/oras-go/v2"
@@ -640,41 +639,17 @@ func createSingleLayerOCIImage(t *testing.T, data []byte, ref string) ([]byte, *
 	var buf bytes.Buffer
 	w := tar.NewOCILayoutWriter(&buf)
 
-	desc := ociImageSpecV1.Descriptor{}
-	desc.Digest = digest.FromBytes(data)
-	desc.Size = int64(len(data))
-	desc.MediaType = ociImageSpecV1.MediaTypeImageLayer
+	desc := content.NewDescriptorFromBytes(ociImageSpecV1.MediaTypeImageLayer, data)
+	r.NoError(w.Push(t.Context(), desc, bytes.NewReader(data)))
+
+	manifest, err := oras.PackManifest(t.Context(), w, oras.PackManifestVersion1_1, ociImageSpecV1.MediaTypeImageLayer, oras.PackManifestOptions{
+		Layers: []ociImageSpecV1.Descriptor{desc},
+	})
+	r.NoError(err)
 
 	r.NoError(w.Push(t.Context(), desc, bytes.NewReader(data)))
 
-	configRaw, err := json.Marshal(map[string]string{})
-	r.NoError(err)
-	configDesc := ociImageSpecV1.Descriptor{
-		Digest:    digest.FromBytes(configRaw),
-		Size:      int64(len(configRaw)),
-		MediaType: "application/json",
-	}
-	r.NoError(w.Push(t.Context(), configDesc, bytes.NewReader(configRaw)))
-
-	manifest := ociImageSpecV1.Manifest{
-		Versioned: specs.Versioned{SchemaVersion: 2},
-		MediaType: ociImageSpecV1.MediaTypeImageManifest,
-		Config:    configDesc,
-		Layers: []ociImageSpecV1.Descriptor{
-			desc,
-		},
-	}
-	manifestRaw, err := json.Marshal(manifest)
-	r.NoError(err)
-
-	manifestDesc := ociImageSpecV1.Descriptor{
-		Digest:    digest.FromBytes(manifestRaw),
-		Size:      int64(len(manifestRaw)),
-		MediaType: ociImageSpecV1.MediaTypeImageManifest,
-	}
-	r.NoError(w.Push(t.Context(), manifestDesc, bytes.NewReader(manifestRaw)))
-
-	r.NoError(w.Tag(t.Context(), manifestDesc, ref))
+	r.NoError(w.Tag(t.Context(), manifest, ref))
 
 	r.NoError(w.Close())
 
