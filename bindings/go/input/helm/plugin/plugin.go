4a62d28c14afb2b46087604c9b0bd9177cc61670
Gergely Brautigam <gergely.brautigam@sap.com>
2025-08-12 10:04:48 +0200

    feat: add a binary plugin for helm input
    Signed-off-by: Gergely Brautigam <gergely.brautigam@sap.com>

--- bindings/go/input/helm/plugin/plugin.go
+++ bindings/go/input/helm/plugin/plugin.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"log/slog"
 	"os"
 
@@ -126,76 +128,36 @@ func processHelmResource(ctx context.Context, request *v1.ProcessResourceInputRe
 	}
 
 	helmMethod := &helm.InputMethod{}
-
 	result, err := helmMethod.ProcessResource(ctx, resource, credentials)
 	if err != nil {
 		return nil, fmt.Errorf("helm input method failed: %w", err)
 	}
 
-	// TODO: Convert the blob result to a file location
-	// For now, create a stub temp file
-	tmp, err := os.CreateTemp("", "helm-resource-*.tar.gz")
+	tmp, err := os.CreateTemp("", "helm-source-*.tar.gz")
 	if err != nil {
 		return nil, fmt.Errorf("error creating temp file: %w", err)
 	}
-	_ = tmp.Close()
+	defer func() {
+		if cerr := tmp.Close(); cerr != nil {
+			err = errors.Join(err, cerr)
+		}
+	}()
 
-	// TODO: Write result.ProcessedBlobData to temp file
-	// TODO: Extract metadata from Chart.yaml in the helm spec
-	_ = result
+	closer, err := result.ProcessedBlobData.ReadCloser()
+	if err != nil {
+		return nil, fmt.Errorf("error reading blob data: %w", err)
+	}
+
+	if _, err := io.Copy(tmp, closer); err != nil {
+		return nil, fmt.Errorf("error writing temp file: %w", err)
+	}
 
 	return &v1.ProcessResourceInputResponse{
 		Resource: &constructorv1.Resource{
 			ElementMeta: constructorv1.ElementMeta{
 				ObjectMeta: constructorv1.ObjectMeta{
-					Name:    "helm-chart", // TODO: Extract from Chart.yaml
-					Version: "v1.0.0",     // TODO: Extract from Chart.yaml
-				},
-			},
-			Type:     "helmChart",
-			Relation: "local",
-		},
-		Location: &types.Location{
-			LocationType: types.LocationTypeLocalFile,
-			Value:        tmp.Name(),
-		},
-	}, nil
-}
-
-// processHelmSource wraps the helm.InputMethod to process sources
-func processHelmSource(ctx context.Context, request *v1.ProcessSourceInputRequest, credentials map[string]string) (*v1.ProcessSourceInputResponse, error) {
-	// Convert plugin request to internal format
-	// Note: helm.InputMethod doesn't currently have ProcessSource,
-	// but we can use ProcessResource and adapt the result
-	resource := &constructorruntime.Resource{
-		AccessOrInput: constructorruntime.AccessOrInput{
-			Input: request.Source.Input,
-		},
-	}
-
-	helmMethod := &helm.InputMethod{}
-	result, err := helmMethod.ProcessResource(ctx, resource, nil)
-	if err != nil {
-		return nil, fmt.Errorf("helm input method failed: %w", err)
-	}
-
-	// TODO: Convert the blob result to a file location
-	tmp, err := os.CreateTemp("", "helm-source-*.tar.gz")
-	if err != nil {
-		return nil, fmt.Errorf("error creating temp file: %w", err)
-	}
-	_ = tmp.Close()
-
-	// TODO: Write result.ProcessedBlobData to temp file
-	// TODO: Extract metadata from Chart.yaml in the helm spec
-	_ = result
-
-	return &v1.ProcessSourceInputResponse{
-		Source: &constructorv1.Source{
-			ElementMeta: constructorv1.ElementMeta{
-				ObjectMeta: constructorv1.ObjectMeta{
-					Name:    "helm-source", // TODO: Extract from Chart.yaml
-					Version: "v1.0.0",      // TODO: Extract from Chart.yaml
+					Name:    result.ProcessedResource.Name,
+					Version: result.ProcessedResource.Version,
 				},
 			},
 			Type: "helmChart",
@@ -205,4 +167,10 @@ func processHelmSource(ctx context.Context, request *v1.ProcessSourceInputReques
 			Value:        tmp.Name(),
 		},
 	}, nil
+}
+
+// processHelmSource wraps the helm.InputMethod to process sources
+func processHelmSource(ctx context.Context, request *v1.ProcessSourceInputRequest, credentials map[string]string) (_ *v1.ProcessSourceInputResponse, err error) {
+	// TODO: implement
+	return nil, nil
 }
