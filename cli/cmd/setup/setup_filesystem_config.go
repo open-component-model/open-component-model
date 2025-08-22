6d010778d55cef5dd5b3e3c8e8b41f7a7bc84716
Jakob Möller <jakob.moeller@sap.com>
2025-08-22 12:08:49 +0200

    fix(cli): fixup / refactor filesystem config handling
    #### What this PR does / why we need it
    - Refactored working directory handling
    - Improved CLI flag precedence logic to ensure CLI flag values override other configurations effectively.
    - Removed hooks and builder logic to reduce complexity and redundancy.
    - Enhanced unit test to validate new working directory and filesystem setup logic is actually set in filesystem context
    
    These changes improve reliability and simplify the implementation of filesystem configuration while ensuring proper value precedence and behavior. They also fixed a bug in the filesystem config defaulting not applying to the context
    
    Signed-off-by: Jakob Möller <jakob.moeller@sap.com>

--- cli/cmd/setup/setup_filesystem_config.go
+++ cli/cmd/setup/setup_filesystem_config.go
@@ -14,23 +14,19 @@ import (
 )
 
 func overrideTempFolder(cmd *cobra.Command, fsCfg *filesystemv1alpha1.Config, value string) {
-	if value != "" {
-		if fsCfg.TempFolder != "" {
-			slog.WarnContext(cmd.Context(), "temp folder was defined in ocm config with value, will be overwritten by value", slog.String("original", fsCfg.TempFolder), slog.String("new", value))
-		}
-
-		fsCfg.TempFolder = value
+	if fsCfg.TempFolder != "" {
+		slog.WarnContext(cmd.Context(), "temp folder was defined in ocm config with value, will be overwritten by value", slog.String("original", fsCfg.TempFolder), slog.String("new", value))
 	}
+
+	fsCfg.TempFolder = value
 }
 
 func overrideWorkingDirectory(cmd *cobra.Command, fsCfg *filesystemv1alpha1.Config, value string) {
-	if value != "" {
-		if fsCfg.WorkingDirectory != "" {
-			slog.WarnContext(cmd.Context(), "working-directory was defined in ocm config with value, will be overwritten by value", slog.String("original", fsCfg.WorkingDirectory), slog.String("new", value))
-		}
-
-		fsCfg.WorkingDirectory = value
+	if fsCfg.WorkingDirectory != "" {
+		slog.WarnContext(cmd.Context(), "working-directory was defined in ocm config with value, will be overwritten by value", slog.String("original", fsCfg.WorkingDirectory), slog.String("new", value))
 	}
+
+	fsCfg.WorkingDirectory = value
 }
 
 func ensureFilesystemConfig(cmd *cobra.Command, cfg *genericv1.Config, fsCfg *filesystemv1alpha1.Config) {
@@ -43,24 +39,22 @@ func ensureFilesystemConfig(cmd *cobra.Command, cfg *genericv1.Config, fsCfg *fi
 	}
 }
 
-type SetupFilesystemConfigOption func(fsCfg *filesystemv1alpha1.Config)
-
-func WithWorkingDirectory(workingDirectory string) SetupFilesystemConfigOption {
-	return func(fsCfg *filesystemv1alpha1.Config) {
-		fsCfg.WorkingDirectory = workingDirectory
-	}
+type FilesystemConfigOptions struct {
+	WorkingDirectory string
+	TempFolder       string
 }
 
-func WithTempFolder(tempFolder string) SetupFilesystemConfigOption {
-	return func(fsCfg *filesystemv1alpha1.Config) {
-		fsCfg.TempFolder = tempFolder
-	}
+var scheme = runtime.NewScheme()
+
+func init() {
+	scheme.MustRegisterWithAlias(&filesystemv1alpha1.Config{},
+		runtime.NewVersionedType(filesystemv1alpha1.ConfigType, filesystemv1alpha1.Version))
+	scheme.MustRegisterWithAlias(&filesystemv1alpha1.Config{},
+		runtime.NewUnversionedType(filesystemv1alpha1.ConfigType))
 }
 
-// SetupFilesystemConfig sets up file system configuration entity.
-func SetupFilesystemConfig(cmd *cobra.Command, opts ...SetupFilesystemConfigOption) {
-	var err error
-
+// FilesystemConfig sets up file system configuration entity.
+func FilesystemConfig(cmd *cobra.Command, opts FilesystemConfigOptions) {
 	ocmCtx := ocmctx.FromContext(cmd.Context())
 	cfg := ocmCtx.Configuration()
 	var fsCfg *filesystemv1alpha1.Config
@@ -75,31 +69,20 @@ func SetupFilesystemConfig(cmd *cobra.Command, opts ...SetupFilesystemConfigOpti
 			fsCfg = _fsCfg
 		}
 	}
+	_, _ = scheme.DefaultType(fsCfg)
 
-	for _, opt := range opts {
-		opt(fsCfg)
-	}
-
-	// CLI flag takes precedence over the config file
-	var tempFolderValue string
+	// Manually set CLI flags takes precedence over the config file or over any passed options
+	// (i.e. from child commands)
 	if flag := cmd.Flags().Lookup(ocmcmd.TempFolderFlag); flag != nil && flag.Changed {
-		tempFolderValue, err = cmd.Flags().GetString(ocmcmd.TempFolderFlag)
-		if err != nil {
-			slog.DebugContext(cmd.Context(), "could not read temp folder flag value", slog.String("error", err.Error()))
-		}
+		overrideTempFolder(cmd, fsCfg, flag.Value.String())
+	} else if opts.TempFolder != "" {
+		overrideTempFolder(cmd, fsCfg, opts.TempFolder)
 	}
-	var workingDirectoryValue string
+
 	if flag := cmd.Flags().Lookup(ocmcmd.WorkingDirectoryFlag); flag != nil && flag.Changed {
-		workingDirectoryValue, err = cmd.Flags().GetString(ocmcmd.WorkingDirectoryFlag)
-		if err != nil {
-			slog.DebugContext(cmd.Context(), "could not read working directory flag value", slog.String("error", err.Error()))
-		}
-	}
-	if tempFolderValue != "" {
-		overrideTempFolder(cmd, fsCfg, tempFolderValue)
-	}
-	if workingDirectoryValue != "" {
-		overrideWorkingDirectory(cmd, fsCfg, workingDirectoryValue)
+		overrideWorkingDirectory(cmd, fsCfg, flag.Value.String())
+	} else if opts.WorkingDirectory != "" {
+		overrideWorkingDirectory(cmd, fsCfg, opts.WorkingDirectory)
 	}
 
 	ensureFilesystemConfig(cmd, cfg, fsCfg)
@@ -137,9 +120,8 @@ func addFilesystemConfigToCentralConfig(cmd *cobra.Command, fsCfg *filesystemv1a
 	if cfg == nil {
 		return fmt.Errorf("no central configuration available")
 	}
-
 	raw := &runtime.Raw{}
-	if err := genericv1.Scheme.Convert(fsCfg, raw); err != nil {
+	if err := scheme.Convert(fsCfg, raw); err != nil {
 		return fmt.Errorf("failed to convert filesystem config to raw: %w", err)
 	}
 	cfg.Configurations = append(cfg.Configurations, raw)
