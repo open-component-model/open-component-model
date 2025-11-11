75ef1cff4c47dba5a9bde7bcc74e552003e5ae91
Frederic Wilhelm <frederic.wilhelm@sap.com>
2025-11-11 15:10:57 +0100

    add base structure for new command
    Signed-off-by: Frederic Wilhelm <frederic.wilhelm@sap.com>

---
+++ cli/cmd/plugins/get/cmd.go
@@ -0,0 +1,36 @@
+package get
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+)
+
+const (
+	FlagRegistry = "registry"
+	FlagOutput   = "output"
+	FlagVersion  = "version"
+)
+
+// pluginDirectoryDefault contains all plugins for ocm.
+// TODO: Check if const would be better suited as this is used in cmd/download/plugin/cmd.go as well.
+var pluginDirectoryDefault = filepath.Join(os.Getenv("HOME"), ".config", "ocm", "plugins")
+
+func New() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:               "get <plugin-component-version>",
+		Short:             "Download specified plugin binary from a registry.",
+		Args:              cobra.ExactArgs(1),
+		Long:              ``,
+		Example:           `  # Download a plugin binary from a registry.`,
+		RunE:              GetPlugin,
+		DisableAutoGenTag: true,
+	}
+
+	return cmd
+}
+
+func GetPlugin(cmd *cobra.Command, args []string) error {
+	return nil
+}
