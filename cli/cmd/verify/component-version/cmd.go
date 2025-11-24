cb62a3cfd477fb544931e879e156e4c54be8a026
Matthias Bruns <git@matthiasbruns.com>
2025-11-24 21:45:21 +0100

    feat: rename credential resolution errors
    On-behalf-of: SAP <matthias.bruns@sap.com>
    
    Signed-off-by: Matthias Bruns <git@matthiasbruns.com>

--- cli/cmd/verify/component-version/cmd.go
+++ cli/cmd/verify/component-version/cmd.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/spf13/cobra"
 	"golang.org/x/sync/errgroup"
+
 	"ocm.software/open-component-model/bindings/go/credentials"
 	descruntime "ocm.software/open-component-model/bindings/go/descriptor/runtime"
 	ctfv1 "ocm.software/open-component-model/bindings/go/oci/spec/repository/v1/ctf"
@@ -233,7 +234,7 @@ func VerifyComponentVersion(cmd *cobra.Command, args []string) error {
 			var creds map[string]string
 			if consumerID, err := handler.GetVerifyingCredentialConsumerIdentity(egctx, signature, verifierSpec); err == nil {
 				if creds, err = credentialGraph.Resolve(egctx, consumerID); err != nil {
-					if errors.Is(err, credentials.ErrCredentialsNotFound) {
+					if errors.Is(err, credentials.ErrNotFound) {
 						logger.DebugContext(egctx, "could not resolve credentials for verification", "error", err.Error())
 					} else {
 						return fmt.Errorf("resolving credentials for verification failed: %w", err)
