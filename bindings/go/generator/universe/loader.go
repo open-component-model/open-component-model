e6a37d8b8e6a7d6d652c7ef43c268101875c3485
Jakob Möller <contact@jakob-moeller.com>
2025-11-26 11:27:20 +0100

    chore: embed JSON schemas and update dependencies
    Added embedded schemas for `Raw` and `Type` types using `//go:embed`. Updated dependencies in `go.mod` and `go.sum`, including adding `golang.org/x/tools`, `golang.org/x/mod`, and `golang.org/x/sync`. Enhanced the `Universe` loader to include OCM Runtime package with improved handling using `golang.org/x/tools/go/packages`. That ensures the tests can use runtime objects in the universe at all times
    
    Signed-off-by: Jakob Möller <contact@jakob-moeller.com>

--- bindings/go/generator/universe/loader.go
+++ bindings/go/generator/universe/loader.go
@@ -3,15 +3,19 @@ package universe
 import (
 	"bufio"
 	"errors"
+	"fmt"
 	"go/parser"
 	"go/token"
+	"log/slog"
 	"os"
 	"path/filepath"
 	"strings"
+
+	"golang.org/x/tools/go/packages"
 )
 
 // Build loads all Go files from the given root directories and
-// populates a Universe with import maps and discovered structs.
+// populates the Universe with import maps and discovered structs.
 func Build(roots []string) (*Universe, error) {
 	u := New()
 	fset := token.NewFileSet()
@@ -42,6 +46,11 @@ func Build(roots []string) (*Universe, error) {
 		if err != nil {
 			return nil, err
 		}
+	}
+
+	// Always try to load the OCM Runtime package to have core types available.
+	if err := u.LoadFromGoImport(RuntimePackage); err != nil {
+		slog.Warn("Cannot load OCM runtime package", "package", RuntimePackage, "error", err)
 	}
 
 	return u, nil
@@ -105,4 +114,30 @@ func readModulePath(path string) (string, error) {
 	}
 
 	return "", errors.New("module path not found in " + path)
+}
+
+func (u *Universe) LoadFromGoImport(importPath string) error {
+	cfg := &packages.Config{
+		Mode: packages.NeedSyntax | packages.NeedTypes | packages.NeedTypesInfo | packages.NeedFiles,
+	}
+
+	pkgs, err := packages.Load(cfg, importPath)
+	if err != nil {
+		return err
+	}
+	if len(pkgs) == 0 {
+		return fmt.Errorf("no package found for %s", importPath)
+	}
+
+	for _, pkg := range pkgs {
+		for _, file := range pkg.Syntax {
+			// pkg.Fset is always available for packages.Load
+			pos := pkg.Fset.Position(file.Pos())
+			filePath := pos.Filename
+			u.RecordImports(filePath, file)
+			u.RegisterTypes(filePath, file)
+		}
+	}
+
+	return nil
 }
