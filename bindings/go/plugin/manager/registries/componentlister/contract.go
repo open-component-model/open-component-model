5bf37071929001450eecf80a68b53838e51659f2
Fabian Burth <fabian.burth@sap.com>
2025-11-27 10:41:25 +0100

    feat(contract): add Builtin interfaces for plugin registration
    Introduce Builtin interfaces to allow plugin registries to register
    internal plugins without requiring callers to explicitly provide a
    scheme with their supported types.
    
    Signed-off-by: Fabian Burth <fabian.burth@sap.com>

--- bindings/go/plugin/manager/registries/componentlister/contract.go
+++ bindings/go/plugin/manager/registries/componentlister/contract.go
@@ -16,6 +16,12 @@ type InternalComponentListerPluginContract interface {
 	GetComponentListerCredentialConsumerIdentity(ctx context.Context, repositorySpecification runtime.Typed) (runtime.Identity, error)
 }
 
+// The BuiltinComponentListerPluginContract has the primary purpose to allow plugin
+// registries to register internal plugins without requiring callers to
+// explicitly provide a scheme with their supported types.
+// A scheme is mapping types to their go types. As the go types of external
+// plugins are not compiled in, they cannot have a scheme and therefore, cannot
+// implement this interface.
 type BuiltinComponentListerPluginContract interface {
 	InternalComponentListerPluginContract
 	GetComponentVersionRepositoryScheme() *runtime.Scheme
