c2e82904a5d30ad979b6ad9ca53dcaf6acfecad3
Fabian Burth <fabian.burth@sap.com>
2025-11-28 12:46:47 +0100

    feat(plugin): introduce typed plugin capabilities (#1273)
    <!-- markdownlint-disable MD041 -->
    #### What this PR does / why we need it
    Introduce typed plugin capabilities and update dependecies
    
    - Added typed plugin capabilities to enhance plugin functionality.
    - Updated go.mod to use runtime v0.0.3.
    - Introduced new capability specifications for various plugin types.
    
    This is a preparation to enable plugins to communicate complex JSON
    schema input to output mappings.
    
    For example, now a plugin could specify that an
    oci access -> helm access
    git access -> local blob
    
    Other plugins might not need such complex mappings (e.g. ocm
    repository). Since the capability specs are typed, they can be tailored
    towards each capabilities needs.
    
    #### Which issue(s) this PR fixes
    <!--
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Contributes to
    https://github.com/open-component-model/ocm-project/issues/760
    
    ---------
    
    Signed-off-by: Fabian Burth <fabian.burth@sap.com>
    Co-authored-by: Jakob Möller <jakob.moeller@sap.com>

---
+++ bindings/go/plugin/manager/contracts/ocmrepository/v1/capability.go
@@ -0,0 +1,28 @@
+package v1
+
+import (
+	"ocm.software/open-component-model/bindings/go/plugin/manager/types"
+	"ocm.software/open-component-model/bindings/go/runtime"
+)
+
+const ComponentVersionRepositoryPluginType types.PluginType = "componentVersionRepository"
+
+var Scheme *runtime.Scheme
+
+func init() {
+	Scheme = runtime.NewScheme()
+	Scheme.MustRegisterWithAlias(&CapabilitySpec{}, runtime.NewUnversionedType(string(ComponentVersionRepositoryPluginType)))
+}
+
+// CapabilitySpec specifies the supported types of a plugin for
+// a particular capability type.
+// +k8s:deepcopy-gen:interfaces=ocm.software/open-component-model/bindings/go/runtime.Typed
+// +k8s:deepcopy-gen=true
+// +ocm:typegen=true
+type CapabilitySpec struct {
+	Type runtime.Type `json:"type"`
+	// TODO(fabianburth): customize / optimize for ocm repository
+	//  currently, it uses the general types.Type, but we might want to tailor this
+	//  to ocm repository specifically.
+	SupportedRepositorySpecTypes []types.Type `json:"supportedRepositorySpecTypes"`
+}
