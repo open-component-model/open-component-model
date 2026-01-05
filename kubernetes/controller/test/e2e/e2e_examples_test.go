4901b2d6f424bed5da93339600163bb976b14f76
Matthias Bruns <git@matthiasbruns.com>
2026-01-05 18:22:01 +0100

    feat: add applyset tests
    On-behalf-of: SAP <matthias.bruns@sap.com>
    Signed-off-by: Matthias Bruns <git@matthiasbruns.com>

--- kubernetes/controller/test/e2e/e2e_examples_test.go
+++ kubernetes/controller/test/e2e/e2e_examples_test.go
@@ -71,6 +71,8 @@ var _ = Describe("controller", func() {
 				name := ""
 
 				if slices.Contains(files, K8sManifest) {
+					// Delete first to ensure idempotency across multiple test runs
+					_ = utils.DeleteServiceAccountClusterAdmin(ctx, "ocm-k8s-toolkit-controller-manager")
 					Expect(utils.MakeServiceAccountClusterAdmin(ctx, "ocm-k8s-toolkit-system", "ocm-k8s-toolkit-controller-manager")).To(Succeed())
 				}
 				if slices.Contains(files, Rgd) {
@@ -116,6 +118,12 @@ var _ = Describe("controller", func() {
 					timeout,
 					"pod", "-l", "app.kubernetes.io/name="+example.Name()+"-podinfo",
 				)).To(Succeed())
+
+				By("verifying ApplySet labels on deployer (parent)")
+				verifyDeployerApplySetLabelsAndAnnotations(ctx, example.Name())
+
+				By("verifying ApplySet labels on deployed resources")
+				verifyDeployedResourcesApplySetLabels(ctx, example.Name())
 
 				// Check for configuration and localization
 				if strings.HasSuffix(example.Name(), "-configuration-localization") {
