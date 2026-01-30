a5e4b63bf70891dbd47f39619fc0a6500b041c51
Matthias Bruns <git@matthiasbruns.com>
2026-01-30 12:18:31 +0100

    feat: oci artifact bindings
    On-behalf-of: SAP <matthias.bruns@sap.com>
    Signed-off-by: Matthias Bruns <git@matthiasbruns.com>

--- bindings/go/oci/spec/transformation/v1alpha1/scheme.go
+++ bindings/go/oci/spec/transformation/v1alpha1/scheme.go
@@ -11,10 +11,14 @@ var (
 	OCIAddComponentVersionV1alpha1 = runtime.NewVersionedType(OCIAddComponentVersionType, Version)
 	OCIAddLocalResourceV1alpha1    = runtime.NewVersionedType(OCIAddLocalResourceType, Version)
 	OCIGetLocalResourceV1alpha1    = runtime.NewVersionedType(OCIGetLocalResourceType, Version)
+	OCIAddOCIArtifactV1alpha1      = runtime.NewVersionedType(OCIAddOCIArtifactType, Version)
+	OCIGetOCIArtifactV1alpha1      = runtime.NewVersionedType(OCIGetOCIArtifactType, Version)
 	CTFGetComponentVersionV1alpha1 = runtime.NewVersionedType(CTFGetComponentVersionType, Version)
 	CTFAddComponentVersionV1alpha1 = runtime.NewVersionedType(CTFAddComponentVersionType, Version)
 	CTFAddLocalResourceV1alpha1    = runtime.NewVersionedType(CTFAddLocalResourceType, Version)
 	CTFGetLocalResourceV1alpha1    = runtime.NewVersionedType(CTFGetLocalResourceType, Version)
+	CTFAddOCIArtifactV1alpha1      = runtime.NewVersionedType(CTFAddOCIArtifactType, Version)
+	CTFGetOCIArtifactV1alpha1      = runtime.NewVersionedType(CTFGetOCIArtifactType, Version)
 )
 
 func init() {
@@ -22,8 +26,12 @@ func init() {
 	Scheme.MustRegisterWithAlias(&OCIAddComponentVersion{}, OCIAddComponentVersionV1alpha1)
 	Scheme.MustRegisterWithAlias(&OCIAddLocalResource{}, OCIAddLocalResourceV1alpha1)
 	Scheme.MustRegisterWithAlias(&OCIGetLocalResource{}, OCIGetLocalResourceV1alpha1)
+	Scheme.MustRegisterWithAlias(&OCIAddOCIArtifact{}, OCIAddOCIArtifactV1alpha1)
+	Scheme.MustRegisterWithAlias(&OCIGetOCIArtifact{}, OCIGetOCIArtifactV1alpha1)
 	Scheme.MustRegisterWithAlias(&CTFGetComponentVersion{}, CTFGetComponentVersionV1alpha1)
 	Scheme.MustRegisterWithAlias(&CTFAddComponentVersion{}, CTFAddComponentVersionV1alpha1)
 	Scheme.MustRegisterWithAlias(&CTFAddLocalResource{}, CTFAddLocalResourceV1alpha1)
 	Scheme.MustRegisterWithAlias(&CTFGetLocalResource{}, CTFGetLocalResourceV1alpha1)
+	Scheme.MustRegisterWithAlias(&CTFAddOCIArtifact{}, CTFAddOCIArtifactV1alpha1)
+	Scheme.MustRegisterWithAlias(&CTFGetOCIArtifact{}, CTFGetOCIArtifactV1alpha1)
 }
