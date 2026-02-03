0101f3e9cffe67abae191834817be20309ccddac
Matthias Bruns <git@matthiasbruns.com>
2026-02-03 13:48:23 +0100

    fix: media-type handling
    On-behalf-of: SAP <matthias.bruns@sap.com>
    Signed-off-by: Matthias Bruns <git@matthiasbruns.com>

--- bindings/go/oci/transformer/get_oci_artifact.go
+++ bindings/go/oci/transformer/get_oci_artifact.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path/filepath"
 
+	"ocm.software/open-component-model/bindings/go/blob"
 	"ocm.software/open-component-model/bindings/go/blob/filesystem"
 	descriptor "ocm.software/open-component-model/bindings/go/descriptor/runtime"
 	v2 "ocm.software/open-component-model/bindings/go/descriptor/v2"
@@ -13,6 +14,12 @@ import (
 	"ocm.software/open-component-model/bindings/go/oci/spec/transformation/v1alpha1"
 	"ocm.software/open-component-model/bindings/go/runtime"
 )
+
+const ociMediaType = "application/vnd.ocm.software.oci.layout.v1+tar+gzip"
+
+var mediaTypExtMap = map[string]string{
+	ociMediaType: "tar.gz",
+}
 
 // GetOCIArtifact is a transformer that retrieves OCI artifacts from remote registries
 // and buffers them to files.
@@ -46,13 +53,24 @@ func (t *GetOCIArtifact) Transform(ctx context.Context, step runtime.Typed) (run
 
 	blobContent, err := t.Repository.DownloadResource(ctx, targetResource)
 	if err != nil {
-		return nil, fmt.Errorf("failed downloading OCI artifact %v %w", resource, err)
+		return nil, fmt.Errorf("failed downloading OCI artifact %v %w", resource.ToIdentity(), err)
+	}
+
+	mediaType := ""
+	switch v := blobContent.(type) {
+	case blob.MediaTypeAware:
+		mediaType, _ = v.MediaType()
+	}
+
+	fileExt, ok := mediaTypExtMap[mediaType]
+	if !ok {
+		return nil, fmt.Errorf("unsupported media type %q for OCI artifact", mediaType)
 	}
 
 	// Determine output path
 	if outputPath == "" {
 		// Create a temporary file
-		tempFile, err := os.CreateTemp("", "oci-artifact-*.tar")
+		tempFile, err := os.CreateTemp("", fmt.Sprintf("oci-artifact-*.%s", fileExt))
 		if err != nil {
 			return nil, fmt.Errorf("failed creating temporary file: %w", err)
 		}
