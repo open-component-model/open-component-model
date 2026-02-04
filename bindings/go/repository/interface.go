08b9349fef4c62cf55b6ee2378f37164251cdb4e
Matthias Bruns <git@matthiasbruns.com>
2026-02-04 13:05:16 +0100

    BREAKING CHANGE: update ResourceRepository to require credentials as a param
    On-behalf-of: SAP <matthias.bruns@sap.com>
    Signed-off-by: Matthias Bruns <git@matthiasbruns.com>

--- bindings/go/repository/interface.go
+++ bindings/go/repository/interface.go
@@ -98,13 +98,16 @@ type LocalSourceRepository interface {
 // ResourceRepository defines the interface for storing and retrieving OCM resources
 // independently of component versions from a store implementation.
 type ResourceRepository interface {
+	// GetResourceCredentialConsumerIdentity resolves the identity of the given [descriptor.Resource] to use for credential resolution.
+	GetResourceCredentialConsumerIdentity(ctx context.Context, resource *descriptor.Resource) (runtime.Identity, error)
 	// UploadResource uploads a [descriptor.Resource] to the repository.
 	// Returns the updated resource with repository-specific information.
 	// The resource must be referenced in the component descriptor.
-	UploadResource(ctx context.Context, res *descriptor.Resource, content blob.ReadOnlyBlob) (resourceAfterUpload *descriptor.Resource, err error)
-
+	// The credentials map must contain necessary authentication information to access the resource.
+	UploadResource(ctx context.Context, res *descriptor.Resource, content blob.ReadOnlyBlob, credentials map[string]string) (resourceAfterUpload *descriptor.Resource, err error)
 	// DownloadResource downloads and verifies the integrity of a [descriptor.Resource] from the repository.
-	DownloadResource(ctx context.Context, res *descriptor.Resource) (content blob.ReadOnlyBlob, err error)
+	// The credentials map must contain necessary authentication information to access the resource.
+	DownloadResource(ctx context.Context, res *descriptor.Resource, credentials map[string]string) (blob.ReadOnlyBlob, error)
 }
 
 // SourceRepository defines the interface for storing and retrieving OCM sources
