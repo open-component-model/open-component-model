d73fd7e0cf8fb75aab9b35498e3058b8a5af04fe
Matthias Bruns <git@matthiasbruns.com>
2026-02-04 16:25:57 +0100

    fix: generated code
    On-behalf-of: SAP <matthias.bruns@sap.com>
    Signed-off-by: Matthias Bruns <git@matthiasbruns.com>

--- bindings/go/oci/spec/transformation/v1alpha1/get_oci_artifact.go
+++ bindings/go/oci/spec/transformation/v1alpha1/get_oci_artifact.go
@@ -3,7 +3,6 @@ package v1alpha1
 import (
 	"ocm.software/open-component-model/bindings/go/blob/filesystem/spec/access/v1alpha1"
 	v2 "ocm.software/open-component-model/bindings/go/descriptor/v2"
-	"ocm.software/open-component-model/bindings/go/oci/repository/resource"
 	"ocm.software/open-component-model/bindings/go/runtime"
 )
 
@@ -44,8 +43,6 @@ type GetOCIArtifactOutput struct {
 // +k8s:deepcopy-gen=true
 // +ocm:jsonschema-gen=true
 type GetOCIArtifactSpec struct {
-	// Repository to download the OCI artifact from.
-	Repository *resource.ResourceRepository `json:"repository"`
 	// Resource is the resource descriptor to get the OCI artifact from.
 	Resource *v2.Resource `json:"resource"`
 	// OutputPath is the path where the artifact should be downloaded to.
