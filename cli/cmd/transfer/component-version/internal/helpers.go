7226ca34101e22a5d933b462e2fc3c42dbb1d171
Matthias Bruns <git@matthiasbruns.com>
2026-02-05 08:55:33 +0100

    feat: register in cli
    On-behalf-of: SAP <matthias.bruns@sap.com>
    Signed-off-by: Matthias Bruns <git@matthiasbruns.com>

--- cli/cmd/transfer/component-version/internal/helpers.go
+++ cli/cmd/transfer/component-version/internal/helpers.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"fmt"
 
+	v1 "ocm.software/open-component-model/bindings/go/oci/spec/access/v1"
 	ctfv1 "ocm.software/open-component-model/bindings/go/oci/spec/repository/v1/ctf"
 	"ocm.software/open-component-model/bindings/go/oci/spec/repository/v1/oci"
 	ociv1alpha1 "ocm.software/open-component-model/bindings/go/oci/spec/transformation/v1alpha1"
@@ -60,6 +61,44 @@ func ChooseAddLocalResourceType(repo runtime.Typed) runtime.Type {
 		return ociv1alpha1.OCIAddLocalResourceV1alpha1
 	case *ctfv1.Repository:
 		return ociv1alpha1.CTFAddLocalResourceV1alpha1
+	default:
+		panic(fmt.Sprintf("unknown repository type %T", repo))
+	}
+}
+
+func ChooseGetOCIArtifactType(repo runtime.Typed) runtime.Type {
+	switch repo.(type) {
+	case *oci.Repository:
+		return ociv1alpha1.OCIGetOCIArtifactV1alpha1
+	default:
+		panic(fmt.Sprintf("unknown repository type %T", repo))
+	}
+}
+
+// isOCIArtifactAccess checks if the given access specification is for an OCI artifact.
+// It checks for various OCI artifact type names including legacy variants.
+func isOCIArtifactAccess(access *runtime.Raw) bool {
+	if access == nil {
+		return false
+	}
+	return access.Type.Name == v1.LegacyType || access.Type.Name == v1.LegacyType2 || access.Type.Name == v1.LegacyType3
+}
+
+// generateTargetImageReference generates the target OCI image reference for a resource.
+// Format: {baseUrl}/{subPath}/{resourceName}:{resourceVersion}
+// For CTF repositories, returns empty string (artifacts are converted to local blobs).
+func generateTargetImageReference(repo runtime.Typed, resourceName, resourceVersion string) string {
+	switch r := repo.(type) {
+	case *oci.Repository:
+		// Build OCI image reference from repository base URL and subpath
+		baseURL := r.BaseUrl
+		if r.SubPath != "" {
+			baseURL = fmt.Sprintf("%s/%s", r.BaseUrl, r.SubPath)
+		}
+		return fmt.Sprintf("%s/%s:%s", baseURL, resourceName, resourceVersion)
+	case *ctfv1.Repository:
+		// CTF: artifacts will be converted to local blobs, no image reference needed
+		return ""
 	default:
 		panic(fmt.Sprintf("unknown repository type %T", repo))
 	}
