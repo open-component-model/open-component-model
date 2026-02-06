a7256a46043dc5e6f5a77743f6ef2486cec90fb5
Matthias Bruns <git@matthiasbruns.com>
2026-02-06 10:09:14 +0100

    feat: handle oci download & upload and add copymode
    On-behalf-of: SAP <matthias.bruns@sap.com>
    Signed-off-by: Matthias Bruns <git@matthiasbruns.com>

--- cli/cmd/transfer/component-version/internal/helpers.go
+++ cli/cmd/transfer/component-version/internal/helpers.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"fmt"
 
+	v2 "ocm.software/open-component-model/bindings/go/descriptor/v2"
 	v1 "ocm.software/open-component-model/bindings/go/oci/spec/access/v1"
 	ctfv1 "ocm.software/open-component-model/bindings/go/oci/spec/repository/v1/ctf"
 	"ocm.software/open-component-model/bindings/go/oci/spec/repository/v1/oci"
@@ -75,6 +76,13 @@ func ChooseGetOCIArtifactType(repo runtime.Typed) runtime.Type {
 	}
 }
 
+func isLocalBlobAccess(access *runtime.Raw) bool {
+	if access == nil {
+		return false
+	}
+	return access.Type.Name == v2.LocalBlobAccessType
+}
+
 // isOCIArtifactAccess checks if the given access specification is for an OCI artifact.
 // It checks for various OCI artifact type names including legacy variants.
 func isOCIArtifactAccess(access *runtime.Raw) bool {
@@ -82,6 +90,10 @@ func isOCIArtifactAccess(access *runtime.Raw) bool {
 		return false
 	}
 	return access.Type.Name == v1.LegacyType || access.Type.Name == v1.LegacyType2 || access.Type.Name == v1.LegacyType3
+}
+
+func isLocalRelation(resource v2.Resource) bool {
+	return resource.Relation == v2.LocalRelation
 }
 
 // generateTargetImageReference generates the target OCI image reference for a resource.
