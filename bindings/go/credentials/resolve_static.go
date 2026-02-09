2c034a9e2912d0ff1a45ee7f3a0c15afbd5e7a21
Matthias Bruns <github@matthiasbruns.com>
2026-02-09 12:43:31 +0100

    feat: add static credentials resolver (#1736)
    <!-- markdownlint-disable MD041 -->
    #### What this PR does / why we need it
    
    This PR is based on the discussion on
    https://github.com/open-component-model/open-component-model/pull/1730#discussion_r2773439650
    
    It implements the `Resolver` interface and provides a static way to
    resolve `credentials`.
    This `Resolver` does not use the graph. If you have an idea on how to
    integrate static credentials into the dag, I am happy to change the
    implementation.
    
    #### Which issue(s) this PR fixes
    Contributes:
    https://github.com/open-component-model/ocm-project/issues/796
    
    ---------
    
    Signed-off-by: Matthias Bruns <git@matthiasbruns.com>

---
+++ bindings/go/credentials/resolve_static.go
@@ -0,0 +1,34 @@
+package credentials
+
+import (
+	"context"
+	"maps"
+
+	"ocm.software/open-component-model/bindings/go/runtime"
+)
+
+// StaticCredentialsResolver is a simple implementation of the Resolver interface
+// that uses a static map to store credentials.
+type StaticCredentialsResolver struct {
+	staticCredentialsStore map[string]map[string]string
+}
+
+// NewStaticCredentialsResolver creates a new StaticCredentialsResolver with the provided credentials map.
+// The input map should have keys that can be derived from the string representation of runtime.Identity
+// and values that are maps of credential key-value pairs.
+func NewStaticCredentialsResolver(credMap map[string]map[string]string) *StaticCredentialsResolver {
+	credStore := maps.Clone(credMap)
+
+	return &StaticCredentialsResolver{
+		staticCredentialsStore: credStore,
+	}
+}
+
+func (s *StaticCredentialsResolver) Resolve(ctx context.Context, identity runtime.Identity) (map[string]string, error) {
+	creds, ok := s.staticCredentialsStore[identity.String()]
+	if !ok {
+		return nil, ErrNotFound
+	}
+
+	return maps.Clone(creds), nil
+}
