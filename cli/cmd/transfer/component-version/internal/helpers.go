bdc695bb77e66e83fd4732a941d3a5e4b3b8947d
Matthias Bruns <git@matthiasbruns.com>
2026-02-10 14:58:38 +0100

    chore: cleanup
    On-behalf-of: SAP <matthias.bruns@sap.com>
    Signed-off-by: Matthias Bruns <git@matthiasbruns.com>

--- cli/cmd/transfer/component-version/internal/helpers.go
+++ cli/cmd/transfer/component-version/internal/helpers.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	v2 "ocm.software/open-component-model/bindings/go/descriptor/v2"
-	v1 "ocm.software/open-component-model/bindings/go/oci/spec/access/v1"
 	ctfv1 "ocm.software/open-component-model/bindings/go/oci/spec/repository/v1/ctf"
 	"ocm.software/open-component-model/bindings/go/oci/spec/repository/v1/oci"
 	ociv1alpha1 "ocm.software/open-component-model/bindings/go/oci/spec/transformation/v1alpha1"
@@ -56,42 +55,6 @@ func ChooseAddLocalResourceType(repo runtime.Typed) runtime.Type {
 	}
 }
 
-func isLocalBlobAccess(access *runtime.Raw) bool {
-	if access == nil {
-		return false
-	}
-	return access.Type.Name == v2.LocalBlobAccessType
-}
-
-// isOCIArtifactAccess checks if the given access specification is for an OCI artifact.
-// It checks for various OCI artifact type names including legacy variants.
-func isOCIArtifactAccess(access *runtime.Raw) bool {
-	if access == nil {
-		return false
-	}
-	return access.Type.Name == v1.LegacyType || access.Type.Name == v1.LegacyType2 || access.Type.Name == v1.LegacyType3
-}
-
 func isLocalRelation(resource v2.Resource) bool {
 	return resource.Relation == v2.LocalRelation
-}
-
-// generateTargetImageReference generates the target OCI image reference for a resource.
-// Format: {baseUrl}/{subPath}/{resourceName}:{resourceVersion}
-// For CTF repositories, returns empty string (artifacts are converted to local blobs).
-func generateTargetImageReference(repo runtime.Typed, resourceName, resourceVersion string) string {
-	switch r := repo.(type) {
-	case *oci.Repository:
-		// Build OCI image reference from repository base URL and subpath
-		baseURL := r.BaseUrl
-		if r.SubPath != "" {
-			baseURL = fmt.Sprintf("%s/%s", r.BaseUrl, r.SubPath)
-		}
-		return fmt.Sprintf("%s/%s:%s", baseURL, resourceName, resourceVersion)
-	case *ctfv1.Repository:
-		// CTF: artifacts will be converted to local blobs, no image reference needed
-		return ""
-	default:
-		panic(fmt.Sprintf("unknown repository type %T", repo))
-	}
 }
