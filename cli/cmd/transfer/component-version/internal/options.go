f4340f5e5a851fb42fd12a08517c978da717c94c
Matthias Bruns <git@matthiasbruns.com>
2026-02-10 15:03:18 +0100

    chore: docs
    On-behalf-of: SAP <matthias.bruns@sap.com>
    Signed-off-by: Matthias Bruns <git@matthiasbruns.com>

---
+++ cli/cmd/transfer/component-version/internal/options.go
@@ -0,0 +1,34 @@
+package internal
+
+type CopyMode int
+
+const (
+	// CopyModeDefault is the default copy mode, which does not download remote references like oci artifacts,
+	// but copies local resources and component references.
+	CopyModeDefault CopyMode = iota
+	// CopyModeLocalResources only copies all resources with a local relation, including oci artifacts.
+	CopyModeLocalResources
+	// CopyModeAllResources copies all resources, including remote references like oci artifacts.
+	CopyModeAllResources
+)
+
+type options struct {
+	copyMode  CopyMode
+	recursive bool
+}
+
+type Option func(*options)
+
+// WithCopyMode sets the copy mode for the transfer operation. The copy mode determines which resources are copied during the transfer.
+// The default copy mode (CopyModeDefault) does not download remote references like OCI artifacts, but copies local resources and component references.
+func WithCopyMode(mode CopyMode) func(*options) {
+	return func(o *options) {
+		o.copyMode = mode
+	}
+}
+
+func WithRecursive(recursive bool) func(*options) {
+	return func(o *options) {
+		o.recursive = recursive
+	}
+}
