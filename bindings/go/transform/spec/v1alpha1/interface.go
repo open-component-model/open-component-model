9dc4ad9a901e15c93649228466c1ab8aa7e86b32
Gerald Morrison (SAP) <gerald.morrison@sap.com>
2026-04-15 13:20:56 +0200

    fix(spec): revert premature credential interface change
    Signed-off-by: Gerald Morrison (SAP) <gerald.morrison@sap.com>

--- bindings/go/transform/spec/v1alpha1/interface.go
+++ bindings/go/transform/spec/v1alpha1/interface.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"ocm.software/open-component-model/bindings/go/cel/jsonschema/decl"
+	"ocm.software/open-component-model/bindings/go/credentials"
 	"ocm.software/open-component-model/bindings/go/runtime"
 	"ocm.software/open-component-model/bindings/go/transform/spec/v1alpha1/meta"
 )
@@ -13,8 +14,7 @@ type Transformation interface {
 	NestedTypedFields() []string
 	NewDeclType(nestedTypedFields map[string]runtime.Type) (*decl.Type, error)
 	FromGeneric(generic *GenericTransformation) error
-	GetCredentialConsumerIdentities(ctx context.Context) (map[string]runtime.Identity, error)
-	Transform(ctx context.Context, credentials map[string]map[string]string) (map[string]any, error)
+	Transform(ctx context.Context, credentialProvider credentials.Resolver) (map[string]any, error)
 }
 
 type Transformer interface {
