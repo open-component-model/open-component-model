78e35100f4a5f8beb824059f8a9a94b91d387fa4
Gerald Morrison (SAP) <gerald.morrison@sap.com>
2026-04-21 17:41:21 +0200

    refactor(cli): rename builtin cosign plugin package to sigstore
    Align package naming with the sigstore handler module. Update all
    imports and references in builtin.go and setup.go.
    
    Signed-off-by: Gerald Morrison (SAP) <gerald.morrison@sap.com>
    Signed-off-by: Gerald Morrison (SAP) <gerald.morrison@sap.com>
    On-behalf-of: Gerald Morrison (SAP) <gerald.morrison@sap.com>
    Signed-off-by: Gerald Morrison (SAP) <gerald.morrison@sap.com>

--- cli/internal/plugin/builtin/sigstore/register.go
+++ cli/internal/plugin/builtin/sigstore/register.go
@@ -1,11 +1,11 @@
-package cosign
+package sigstore
 
 import (
 	"ocm.software/open-component-model/bindings/go/plugin/manager/registries/signinghandler"
 	"ocm.software/open-component-model/bindings/go/sigstore/signing/handler"
 )
 
-// Register registers the cosign CLI-based Sigstore signing handler with the signing registry.
+// Register registers the Sigstore signing handler with the signing registry.
 func Register(signingHandlerRegistry *signinghandler.SigningRegistry) error {
 	return signingHandlerRegistry.RegisterInternalComponentSignatureHandler(handler.New())
 }
