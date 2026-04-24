674d939acd5053170e7c2948cc9833ca6c55c392
Gerald Morrison (SAP) <gerald.morrison@sap.com>
2026-04-24 13:42:55 +0200

    change to table-driven tests
    Signed-off-by: Gerald Morrison (SAP) <gerald.morrison@sap.com>

--- bindings/go/descriptor/v2/scheme_test.go
+++ bindings/go/descriptor/v2/scheme_test.go
@@ -9,34 +9,21 @@ import (
 	"ocm.software/open-component-model/bindings/go/runtime"
 )
 
-func TestScheme_ResolvesUpperCamelCase_LocalBlob(t *testing.T) {
-	obj, err := descriptorv2.Scheme.NewObject(
-		runtime.NewVersionedType(descriptorv2.LocalBlobAccessType, descriptorv2.LocalBlobAccessTypeVersion),
-	)
-	require.NoError(t, err, "Scheme must resolve UpperCamelCase type LocalBlob/v1")
-	require.IsType(t, &descriptorv2.LocalBlob{}, obj, "expected *LocalBlob from Scheme")
-}
-
-func TestScheme_ResolvesLegacy_LocalBlob(t *testing.T) {
-	obj, err := descriptorv2.Scheme.NewObject(
-		runtime.NewVersionedType(descriptorv2.LegacyLocalBlobAccessType, descriptorv2.LocalBlobAccessTypeVersion),
-	)
-	require.NoError(t, err, "Scheme must resolve legacy type localBlob/v1")
-	require.IsType(t, &descriptorv2.LocalBlob{}, obj, "expected *LocalBlob from Scheme")
-}
-
-func TestScheme_ResolvesUnversioned_LocalBlob(t *testing.T) {
-	obj, err := descriptorv2.Scheme.NewObject(
-		runtime.NewUnversionedType(descriptorv2.LocalBlobAccessType),
-	)
-	require.NoError(t, err, "Scheme must resolve unversioned LocalBlob")
-	require.IsType(t, &descriptorv2.LocalBlob{}, obj, "expected *LocalBlob from Scheme")
-}
-
-func TestScheme_ResolvesUnversionedLegacy_LocalBlob(t *testing.T) {
-	obj, err := descriptorv2.Scheme.NewObject(
-		runtime.NewUnversionedType(descriptorv2.LegacyLocalBlobAccessType),
-	)
-	require.NoError(t, err, "Scheme must resolve unversioned legacy localBlob")
-	require.IsType(t, &descriptorv2.LocalBlob{}, obj, "expected *LocalBlob from Scheme")
+func TestScheme_ResolvesLocalBlob(t *testing.T) {
+	cases := []struct {
+		name string
+		typ  runtime.Type
+	}{
+		{"UpperCamelCase versioned", runtime.NewVersionedType(descriptorv2.LocalBlobAccessType, descriptorv2.LocalBlobAccessTypeVersion)},
+		{"legacy versioned", runtime.NewVersionedType(descriptorv2.LegacyLocalBlobAccessType, descriptorv2.LocalBlobAccessTypeVersion)},
+		{"unversioned", runtime.NewUnversionedType(descriptorv2.LocalBlobAccessType)},
+		{"unversioned legacy", runtime.NewUnversionedType(descriptorv2.LegacyLocalBlobAccessType)},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			obj, err := descriptorv2.Scheme.NewObject(tc.typ)
+			require.NoError(t, err)
+			require.IsType(t, &descriptorv2.LocalBlob{}, obj)
+		})
+	}
 }
