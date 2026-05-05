6a96d5790c7e9f9a3c74c877489851a5a4716f5b
Gerald Morrison (SAP) <gerald.morrison@sap.com>
2026-05-05 23:36:22 +0200

    fix(oidc): align register.go with handler.New() signature
    Signed-off-by: Gerald Morrison (SAP) <gerald.morrison@sap.com>

--- cli/internal/plugin/builtin/oidc/register.go
+++ cli/internal/plugin/builtin/oidc/register.go
@@ -8,11 +8,7 @@ import (
 
 // Register registers the Sigstore signing handler with the signing registry.
 func Register(signingHandlerRegistry *signinghandler.SigningRegistry) error {
-	h, err := handler.New()
-	if err != nil {
-		return err
-	}
-	return signingHandlerRegistry.RegisterInternalComponentSignatureHandler(h)
+	return signingHandlerRegistry.RegisterInternalComponentSignatureHandler(handler.New())
 }
 
 // RegisterCredentialPlugin registers the OIDC credential plugin with the credential plugin registry.
