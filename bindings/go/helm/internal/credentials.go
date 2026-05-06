57fd623a14cd0762eccd065c34994906a053d560
Matthias Bruns <github@matthiasbruns.com>
2026-05-06 13:21:51 +0200

    fix(deps): update module ocm.software/open-component-model/bindings/go/oci to v0.0.41 (#2445)

--- bindings/go/helm/internal/credentials.go
+++ bindings/go/helm/internal/credentials.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	helmaccess "ocm.software/open-component-model/bindings/go/helm/spec/access"
-	ocicredentialsspecv1 "ocm.software/open-component-model/bindings/go/oci/spec/credentials/identity/v1"
+	ocicredentialsspecv1 "ocm.software/open-component-model/bindings/go/oci/spec/identity/v1"
 	"ocm.software/open-component-model/bindings/go/runtime"
 )
 
