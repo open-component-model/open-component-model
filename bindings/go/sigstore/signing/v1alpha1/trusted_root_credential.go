936ed758703227321ea8095f8fef5210aef9fc3d
Gerald Morrison (SAP) <gerald.morrison@sap.com>
2026-05-07 21:24:28 +0200

    chore(sigstore): remove unused credential type placeholders
    OIDCIdentityToken struct and CredentialTypeTrustedRoot var were never
    used in any logic. PR #2453 introduces the real runtime.Typed
    implementations from scratch under spec/credentials/.
    
    Signed-off-by: Gerald Morrison (SAP) <gerald.morrison@sap.com>

--- bindings/go/sigstore/signing/v1alpha1/trusted_root_credential.go
+++
@@ -1,7 +0,0 @@
-package v1alpha1
-
-import "ocm.software/open-component-model/bindings/go/runtime"
-
-// CredentialTypeTrustedRoot is the credential type for Sigstore trusted-root material
-// (Fulcio CA chain, Rekor log keys, timestamp authority certificates).
-var CredentialTypeTrustedRoot = runtime.NewVersionedType("TrustedRoot", Version)
