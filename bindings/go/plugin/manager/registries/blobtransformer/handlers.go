ffeb43606a31f5602aa472a589734fcbb40bccc9
Matthias Bruns <git@matthiasbruns.com>
2026-05-20 09:08:28 +0200

    feat: migrate blobtransformer to runtime.Typed
    On-behalf-of: SAP <matthias.bruns@sap.com>
    Signed-off-by: Matthias Bruns <git@matthiasbruns.com>

--- bindings/go/plugin/manager/registries/blobtransformer/handlers.go
+++ bindings/go/plugin/manager/registries/blobtransformer/handlers.go
@@ -15,10 +15,10 @@ import (
 // TransformBlobHandlerFunc is a wrapper around calling the interface method TransformBlobHandler for the plugin.
 // This is a convenience wrapper containing header and query parameter parsing logic that is not important to know for
 // the plugin implementor.
-func TransformBlobHandlerFunc[T runtime.Typed](f func(ctx context.Context, request *v1.TransformBlobRequest[T], credentials map[string]string) (*v1.TransformBlobResponse, error)) http.HandlerFunc {
+func TransformBlobHandlerFunc[T runtime.Typed](f func(ctx context.Context, request *v1.TransformBlobRequest[T], credentials runtime.Typed) (*v1.TransformBlobResponse, error)) http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
 		rawCredentials := []byte(request.Header.Get("Authorization"))
-		credentials := map[string]string{}
+		var credentials runtime.Typed
 		if err := json.Unmarshal(rawCredentials, &credentials); err != nil {
 			plugins.NewError(fmt.Errorf("incorrect authentication header format: %w", err), http.StatusUnauthorized).Write(writer)
 			return
