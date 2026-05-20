86639f70790b5e47db8f990eb66a716891d02a25
Matthias Bruns <git@matthiasbruns.com>
2026-05-20 09:45:44 +0200

    chore: roll back not needed credential binding change
    On-behalf-of: SAP <matthias.bruns@sap.com>
    Signed-off-by: Matthias Bruns <git@matthiasbruns.com>
    On-behalf-of: @SAP <jakob.moeller@sap.com>

--- bindings/go/credentials/resolve_static.go
+++ bindings/go/credentials/resolve_static.go
@@ -11,32 +11,13 @@ import (
 // StaticCredentialsResolver is a simple implementation of the Resolver interface
 // that uses a static map to store credentials.
 type StaticCredentialsResolver struct {
-	staticCredentialsStore map[string]runtime.Typed
+	staticCredentialsStore map[string]map[string]string
 }
 
 // NewStaticCredentialsResolver creates a new StaticCredentialsResolver with the provided credentials map.
 // The input map should have keys that can be derived from the string representation of runtime.Identity
 // and values that are maps of credential key-value pairs.
 func NewStaticCredentialsResolver(credMap map[string]map[string]string) *StaticCredentialsResolver {
-	credsStore := map[string]runtime.Typed{}
-
-	for k, v := range credMap {
-		credsStore[k] = &v1.DirectCredentials{
-			Type:       runtime.NewVersionedType(v1.CredentialsType, v1.Version),
-			Properties: maps.Clone(v),
-		}
-	}
-
-	return &StaticCredentialsResolver{
-		staticCredentialsStore: credsStore,
-	}
-}
-
-// NewStaticCredentialsResolverFromTyped creates a new StaticCredentialsResolver with the provided credentials.
-// The input map should have keys that can be derived from the string representation of runtime.Identity
-// and values that are runtime.Typed credentials.
-// TODO(matthiasbruns): Update tests to use NewStaticCredentialsResolverFromTyped instead of NewStaticCredentialsResolver
-func NewStaticCredentialsResolverFromTyped(credMap map[string]runtime.Typed) *StaticCredentialsResolver {
 	credStore := maps.Clone(credMap)
 
 	return &StaticCredentialsResolver{
@@ -49,6 +30,8 @@ func (s *StaticCredentialsResolver) Resolve(_ context.Context, identity runtime.
 	if !ok {
 		return nil, ErrNotFound
 	}
-	// return cloned creds
-	return creds.DeepCopyTyped(), nil
+	return &v1.DirectCredentials{
+		Type:       runtime.NewVersionedType(v1.CredentialsType, v1.Version),
+		Properties: maps.Clone(creds),
+	}, nil
 }
