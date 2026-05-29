78bb1611fb0119733410c62621b5d012443bbe36
Jakob Möller <contact@jakob-moeller.com>
2026-05-29 12:08:27 +0200

    refactor: drop redundant scheme construction in gpg register
    handler.New ignores the passed scheme; GetSigningHandlerScheme()
    on the handler provides its own scheme, which the registry reads
    directly via RegisterInternalComponentSignatureHandler.
    
    Signed-off-by: Jakob Möller <contact@jakob-moeller.com>
    On-behalf-of: @SAP <jakob.moeller@sap.com>

--- cli/internal/plugin/builtin/gpg/register.go
+++ cli/internal/plugin/builtin/gpg/register.go
@@ -7,21 +7,14 @@ package gpg
 import (
 	filesystemv1alpha1 "ocm.software/open-component-model/bindings/go/configuration/filesystem/v1alpha1/spec"
 	"ocm.software/open-component-model/bindings/go/gpg/signing/handler"
-	v1alpha1 "ocm.software/open-component-model/bindings/go/gpg/spec/signing/v1alpha1"
 	"ocm.software/open-component-model/bindings/go/plugin/manager/registries/signinghandler"
-	"ocm.software/open-component-model/bindings/go/runtime"
 )
 
 func Register(
 	signingHandlerRegistry *signinghandler.SigningRegistry,
 	_ *filesystemv1alpha1.Config,
 ) error {
-	scheme := runtime.NewScheme()
-	if err := scheme.RegisterScheme(v1alpha1.Scheme); err != nil {
-		return err
-	}
-
-	hdlr, err := handler.New(scheme)
+	hdlr, err := handler.New(nil)
 	if err != nil {
 		return err
 	}
