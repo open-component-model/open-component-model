6ff9e06475198d71ff96b5983cbdf1217770ead3
Johannes Neisener <johannes@neisener.de>
2026-06-10 17:46:14 +0200

    Add conversion from `v1` to credentials config
    Signed-off-by: Johannes Neisener <johannes@neisener.de>
    On-behalf-of: @SAP <johannes.neisener@sap.com>

--- bindings/go/credentials/spec/config/runtime/convert_v1.go
+++ bindings/go/credentials/spec/config/runtime/convert_v1.go
@@ -8,23 +8,23 @@ import (
 func ConvertFromV1(config *v1.Config) *Config {
 	return &Config{
 		Type:         config.Type,
-		Repositories: convertRepositories(config.Repositories),
-		Consumers:    convertConsumers(config.Consumers),
+		Repositories: convertFromV1Repositories(config.Repositories),
+		Consumers:    convertFromV1Consumers(config.Consumers),
 	}
 }
 
-func convertConsumers(consumers []v1.Consumer) []Consumer {
+func convertFromV1Consumers(consumers []v1.Consumer) []Consumer {
 	entries := make([]Consumer, len(consumers))
 	for i, consumer := range consumers {
 		entries[i] = Consumer{
-			Identities:  convertIdentities(consumer.Identities),
-			Credentials: convertCredentials(consumer.Credentials),
+			Identities:  deepCopyIdentities(consumer.Identities),
+			Credentials: convertFromV1Credentials(consumer.Credentials),
 		}
 	}
 	return entries
 }
 
-func convertIdentities(identities []runtime.Identity) []runtime.Identity {
+func deepCopyIdentities(identities []runtime.Identity) []runtime.Identity {
 	nidentities := make([]runtime.Identity, len(identities))
 	for i, identity := range identities {
 		nidentities[i] = identity.DeepCopy()
@@ -32,7 +32,7 @@ func convertIdentities(identities []runtime.Identity) []runtime.Identity {
 	return nidentities
 }
 
-func convertCredentials(credentials []*runtime.Raw) []runtime.Typed {
+func convertFromV1Credentials(credentials []*runtime.Raw) []runtime.Typed {
 	entries := make([]runtime.Typed, len(credentials))
 	for i, cred := range credentials {
 		entries[i] = cred.DeepCopy()
@@ -40,11 +40,48 @@ func convertCredentials(credentials []*runtime.Raw) []runtime.Typed {
 	return entries
 }
 
-func convertRepositories(repositories []v1.RepositoryConfigEntry) []RepositoryConfigEntry {
+func convertFromV1Repositories(repositories []v1.RepositoryConfigEntry) []RepositoryConfigEntry {
 	entries := make([]RepositoryConfigEntry, len(repositories))
 	for i, repo := range repositories {
 		entries[i] = RepositoryConfigEntry{
 			Repository: repo.Repository.DeepCopy(),
+		}
+	}
+	return entries
+}
+
+func ConvertToV1(config *Config) *v1.Config {
+	return &v1.Config{
+		Type:         config.Type,
+		Repositories: convertToV1Repositories(config.Repositories),
+		Consumers:    convertToV1Consumers(config.Consumers),
+	}
+}
+
+func convertToV1Consumers(consumers []Consumer) []v1.Consumer {
+	entries := make([]v1.Consumer, len(consumers))
+	for i, consumer := range consumers {
+		entries[i] = v1.Consumer{
+			Identities:  deepCopyIdentities(consumer.Identities),
+			Credentials: convertToV1Credentials(consumer.Credentials),
+		}
+	}
+	return entries
+}
+
+func convertToV1Credentials(credentials []runtime.Typed) []*runtime.Raw {
+	entries := make([]*runtime.Raw, len(credentials))
+	for i, cred := range credentials {
+		entries[i] = cred.(*runtime.Raw).DeepCopy()
+	}
+	return entries
+}
+
+func convertToV1Repositories(repositories []RepositoryConfigEntry) []v1.RepositoryConfigEntry {
+	entries := make([]v1.RepositoryConfigEntry, len(repositories))
+	for i, repo := range repositories {
+		entries[i] = v1.RepositoryConfigEntry{
+			Repository: repo.Repository.(*runtime.Raw).DeepCopy(),
 		}
 	}
 	return entries
