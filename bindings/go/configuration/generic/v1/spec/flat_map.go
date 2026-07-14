ee43133748688ce1f801f4d241612b2d84eb9f00
Johannes Neisener <johannes@neisener.de>
2026-07-14 12:34:56 +0200

    Adjust `FlatMap` merging logic
    Signed-off-by: Johannes Neisener <johannes@neisener.de>
    On-behalf-of: @SAP <johannes.neisener@sap.com>

--- bindings/go/configuration/generic/v1/spec/flat_map.go
+++ bindings/go/configuration/generic/v1/spec/flat_map.go
@@ -1,14 +1,14 @@
 package spec
 
 import (
-	"slices"
-
 	"ocm.software/open-component-model/bindings/go/runtime"
 )
 
 // FlatMap merges the provided configs into a single config.
 // The configurations are merged in the order they are provided.
-// Nested configurations are flattened into a single configuration.
+// Nested configurations are flattened in-place into a single configuration.
+// Resulting FlatMap is ordered in ascending priority, order of files (and inside of files)
+// dictates priority. Later entries override earlier ones.
 // Configuration types are decoded the least effort, and if they are not yet decoded,
 // they will only be loaded in if they are of type ConfigType.
 // All other types will be left as is and taken over.
@@ -16,28 +16,19 @@ func FlatMap(configs ...*Config) *Config {
 	merged := new(Config)
 	merged.Configurations = make([]*runtime.Raw, 0)
 	for _, config := range configs {
-		flattenCandidates := make([]*Config, 0)
-		for _, config := range config.Configurations {
+		for _, entry := range config.Configurations {
 			var cfg Config
-			if err := Scheme.Convert(config, &cfg); err != nil {
-				merged.Configurations = append(merged.Configurations, config)
+			if err := Scheme.Convert(entry, &cfg); err != nil {
+				merged.Configurations = append(merged.Configurations, entry)
 			} else {
-				flattenCandidates = append(flattenCandidates, &cfg)
+				flattened := FlatMap(&cfg)
+				if flattened == nil {
+					return nil
+				}
+				merged.Configurations = append(merged.Configurations, flattened.Configurations...)
 			}
 		}
-
-		cfg := FlatMap(flattenCandidates...)
-		if cfg == nil {
-			return nil
-		}
-
-		merged.Configurations = append(merged.Configurations, cfg.Configurations...)
 	}
 
-	// reverse the order of the configurations to match the order of the input configs
-	// this is important for the order of the configurations to be preserved.
-	// In case of Configuration Sets declared in a Config file the LAST item in the list
-	// should be the one overwriting any preceding items.
-	slices.Reverse(merged.Configurations)
 	return merged
 }
